3c305c7079cbf2efbc1490ac1536333174d6386b
elliotcarey0011 <38214801+elliotcarey0011@users.noreply.github.com>
2018-07-04 18:27:30 +0200

    React Dash Edit

---
+++ src/variables/connecttospreadsheet.jsx
@@ -0,0 +1,10 @@
+import ReactGoogleSheetConnector from "react-google-sheet-connector"
+
+<ReactGoogleSheetConnector clientid={1079019012959-if0m6hvogojbbrctm428h9fh4ur1ntuq.apps.googleusercontent.com}
+    apiKey={AIzaSyAZcWuLyIbn_iCmTALnvtfgQRDoZeRU9O4}
+    spreadsheetId={1_YR4uuTaSychWUuRYbGk3R2KVbgFm3MOL4M5WJeeQdc
+    spinner={ <div className="loading-spinner"/> } >
+    <div>
+    	This content will be rendered once the data has been fetched from the spreadsheet.
+    </div>
+</ReactGoogleSheetConnector>
